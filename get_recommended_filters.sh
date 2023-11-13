7764e9042e22383cc98a9e4f940fce33fbcabf67
kingsmanvn1112 <nguyenhuuvuong11122002@gmail.com>
2023-11-13 21:44:27 +0700

    update

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -5,26 +5,33 @@ touch input.csv
 
 # declare an array of urls
 urls=(
-  https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txt
-  https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_gambling.txt
-  https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt
-  https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
-  https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt
-  https://adaway.org/hosts.txt
-  https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+    https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
+    https://raw.githubusercontent.com/Yhonay/antipopads/master/hosts
+    https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/light-onlydomains.txt
+    https://gitlab.com/fusionjack/adhell3-hosts/-/raw/master/hosts
+    https://abpvn.com/android/abpvn.txt
+    https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt
+    https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+    https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Extension/GoodbyeAds-Xiaomi-Extension.txt
+    https://raw.githubusercontent.com/nextdns/native-tracking-domains/main/domains/xiaomi
+    https://raw.githubusercontent.com/nextdns/native-tracking-domains/main/domains/windows
+    https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt
+    https://malware-filter.gitlab.io/malware-filter/vn-badsite-filter-hosts.txt
+    https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
+    https://small.oisd.nl/domainswild2
 )
 
 # loop through the urls and download each file with curl
 for url in "${urls[@]}"; do
-  # get the file name from the url
-  file=$(basename "$url")
-  # download the file with curl and save it as file.txt
-  curl -o "$file.txt" "$url"
-  # append the file contents to input.csv and add a newline
-  cat "$file.txt" >> input.csv
-  echo "" >> input.csv
-  # remove the file.txt
-  rm "$file.txt"
+    # get the file name from the url
+    file=$(basename "$url")
+    # download the file with curl and save it as file.txt
+    curl -o "$file.txt" "$url"
+    # append the file contents to input.csv and add a newline
+    cat "$file.txt" >> input.csv
+    echo "" >> input.csv
+    # remove the file.txt
+    rm "$file.txt"
 done
 
 # print a message when done
--- get_recommended_whitelist.sh
+++ get_recommended_whitelist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#  
+#
 # Use the provided lists or add you own
 # https://oisd.nl/includedlists/whitelists
 # by creating an empty whitelist.csv file
@@ -7,14 +7,6 @@ touch whitelist.csv
 
 # declare an array of urls
 urls=(
-    https://raw.githubusercontent.com/im-sm/Pi-hole-Torrent-Blocklist/main/all-torrent-trackres.txt
-    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/banks.txt
-    https://raw.githubusercontent.com/hagezi/dns-blocklists/main/whitelist.txt
-    https://raw.githubusercontent.com/TogoFire-Home/AD-Settings/main/Filters/whitelist.txt
-    https://raw.githubusercontent.com/freekers/whitelist/master/domains/whitelist.txt
-    https://raw.githubusercontent.com/DandelionSprout/AdGuard-Home-Whitelist/master/whitelist.txt
-    # Commented out because it looks suspicious
-    # https://www.aadvantageeshopping.com/adBlockWhitelist.php
     https://raw.githubusercontent.com/AdguardTeam/AdGuardSDNSFilter/master/Filters/exclusions.txt
     https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/optional-list.txt
     https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/issues.txt
@@ -30,22 +22,26 @@ urls=(
     https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/sensitive.txt
     https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt
     https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/firefox.txt
-    # Commented out because it whitelists sites including doubleclick.net and ad.atdmt.com
-    # https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt
-
+    https://raw.githubusercontent.com/im-sm/Pi-hole-Torrent-Blocklist/main/all-torrent-trackres.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/banks.txt
+    https://raw.githubusercontent.com/hagezi/dns-blocklists/main/whitelist.txt
+    https://raw.githubusercontent.com/TogoFire-Home/AD-Settings/main/Filters/whitelist.txt
+    https://raw.githubusercontent.com/freekers/whitelist/master/domains/whitelist.txt
+    https://raw.githubusercontent.com/DandelionSprout/AdGuard-Home-Whitelist/master/whitelist.txt
+    https://raw.githubusercontent.com/nextdns/click-tracking-domains/main/domains
 )
 
 # loop through the urls and download each file with curl
 for url in "${urls[@]}"; do
-  # get the file name from the url
-  file=$(basename "$url")
-  # download the file with curl and save it as file.txt
-  curl -o "$file.txt" "$url"
-  # append the file contents to whitelist.csv and add a newline
-  cat "$file.txt" >> whitelist.csv
-  echo "" >> whitelist.csv
-  # remove the file.txt
-  rm "$file.txt"
+    # get the file name from the url
+    file=$(basename "$url")
+    # download the file with curl and save it as file.txt
+    curl -o "$file.txt" "$url"
+    # append the file contents to whitelist.csv and add a newline
+    cat "$file.txt" >> whitelist.csv
+    echo "" >> whitelist.csv
+    # remove the file.txt
+    rm "$file.txt"
 done
 
 # print a message when done
